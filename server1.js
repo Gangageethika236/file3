48c8ec9d10ecc9b5b4125c8165956e6b50813aea
Gangageethika236 <gangageethika6103@gmail.com>
2024-07-06 15:42:50 +0530

    first commit

---
+++ server1.js
@@ -0,0 +1,11 @@
+var http=require('http')
+var fs=require('fs')
+http.createServer(
+    function(req,res){
+        fs.readFile('example.txt',function(error,data){
+            res.write(data)
+            res.end()
+        })
+        
+    }
+).listen(3030)
